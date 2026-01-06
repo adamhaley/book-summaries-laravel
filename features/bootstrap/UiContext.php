b902c7a6ea44ad976698d543c7cdc5d316d90d92
adamhaley <adamhaley@gmail.com>
2026-01-06 09:33:18 -0800

    initial commit

---
+++ features/bootstrap/UiContext.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Features\Bootstrap;
+
+use Behat\Behat\Context\Context;
+
+final class UiContext implements Context
+{
+    // Add step definitions and hooks here.
+}
